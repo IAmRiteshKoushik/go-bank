abb2c24608a42af25477cabb168d215d1170b84a
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-06-27 19:23:43 +0530

    add: CreateAccountRequired struct

--- types.go
+++ types.go
@@ -1,6 +1,9 @@
 package main
 
-import "math/rand/v2"
+import (
+    "math/rand/v2"
+    "time"
+)
 
 // Struct tags will effectively tell the program that if this struct 
 // gets serialized into a JSON then how the field names are going to be
@@ -10,14 +13,23 @@ type Account struct {
     LastName  string    `json:"last_name"`
     Number    int64     `json:"number"`
 	Balance   int64     `json:"balance"`
+    CreatedAt time.Time `json:"created_at"`
+}
+
+type CreateAccountRequest struct {
+    // The reason why we have this type and we are not using the Account 
+    // struct is because we would be setting up the ID, Number and Balance
+    // in the backend and will not be receiving them from the front-end
+    FirstName string `json:"first_name"`
+    LastName string `json:"last_name"`
 }
 
 func NewAccount(firstName, lastName string) *Account {
     return &Account{
-        ID: rand.IntN(10_000),
         FirstName: firstName,
         LastName: lastName,
         Number: int64(rand.IntN(1000000)),
         Balance: 0,
+        CreatedAt: time.Now().UTC(),
     }
 }
