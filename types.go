0dbc307af6e912628cbf2ef4634a6219f1ee1b87
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-06-28 16:44:52 +0530

    Add login functionality with tests

--- types.go
+++ types.go
@@ -3,16 +3,24 @@ package main
 import (
     "math/rand/v2"
     "time"
+
+    "golang.org/x/crypto/bcrypt"
 )
+
+type LoginRequest struct {
+    Number      int64   `json:"number"`
+    Password    string  `json:"password"`
+}
 
 // Struct tags will effectively tell the program that if this struct 
 // gets serialized into a JSON then how the field names are going to be
 type Account struct {
-	ID        int       `json:"id"`
-	FirstName string    `json:"first_name"`
+    ID        int       `json:"id"`
+    FirstName string    `json:"first_name"`
     LastName  string    `json:"last_name"`
     Number    int64     `json:"number"`
-	Balance   int64     `json:"balance"`
+    EncryptedPassword string `json:"-"`
+    Balance   int64     `json:"balance"`
     CreatedAt time.Time `json:"created_at"`
 }
 
@@ -22,6 +30,7 @@ type CreateAccountRequest struct {
     // in the backend and will not be receiving them from the front-end
     FirstName string `json:"first_name"`
     LastName string `json:"last_name"`
+    Password string `json:"password"`
 }
 
 type TransferRequest struct {
@@ -29,13 +38,18 @@ type TransferRequest struct {
     Amount      int     `json:"amount"`
 }
 
-func NewAccount(firstName, lastName string) *Account {
+func NewAccount(firstName, lastName, password string) (*Account, error) {
+    encpw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+    if err != nil {
+        return nil, err
+    }
     return &Account{
         FirstName: firstName,
         LastName: lastName,
         Number: int64(rand.IntN(1000000)),
+        EncryptedPassword: string(encpw),
         Balance: 0,
         CreatedAt: time.Now().UTC(),
-    }
+    }, nil
 }
 
